0db6eed41b0cd6077cd5a1651f49ccdf17ccdab2
Joseph Beck <joebeck03@icloud.com>
2023-02-04 14:21:05 +0000

    Natan's uploading the code back

--- src/co-op-scores.js
+++ src/co-op-scores.js
@@ -1,17 +1,29 @@
 var startTime, endTime, tempScore;
 const coopAddScore = 6;
-
+const coopMultiplier = 0.3;
 
 
 function start() {
   startTime = new Date();
+  return startTime;
 }
-function end() {
+
+function end() { //calculates the total time between a guess/answer
    endTime = new Date();
-   var diffTime = endTime - startTime;
+   var diffTime = endTime - start();
    return diffTime;
 }
 
 function scoresClac() {
+    var timeScore;
+    console.log(end());
+    console.log(timeScore+coopAddScore);
   
 } 
+start();
+
+for(let i = 0; i<30430; i++){
+    console.log(i);
+}
+end();
+scoresClac();
