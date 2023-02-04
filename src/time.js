313cb05d66492dc610b8870fe808d68267f13c1f
Tworye01 <natanwojcik15@gmail.com>
2023-02-04 19:49:49 +0000

    Tidying up a little bit

--- src/time.js
+++ src/time.js
@@ -3,6 +3,7 @@ var startTime, endTime;
 function start() { //starts the timer
     startTime = new Date();
 }
+
 function end() { //calculates the total time between a guess/answer and end of time
      endTime = new Date();
      var diffTime = endTime - startTime;
