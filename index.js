5573c04b8b20c7f3e5aef15d6e1296721bbd24a3
Jacob Padley <jacob5180@hotmail.co.uk>
2023-02-04 12:44:10 +0000

    Created separate main function

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { Client, Events, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
 
+// Setup the discord client
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.once(Events.ClientReady, c => {
-    console.log(`Connected as ${c.user.tag}`);
-});
-
+// Trigger the main function once the client has connected
+client.once(Events.ClientReady, main);
 client.login(token);
+
+function main(c){
+    console.log(c.user.tag);
+}
