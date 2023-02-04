21f20ddf4cca9ee74fa776925d99dcec0785fb3f
Jacob Padley <jacob5180@hotmail.co.uk>
2023-02-04 16:53:30 +0000

    Refactored wordle.js

--- src/wordle.js
+++ src/wordle.js
@@ -1,130 +1,99 @@
+const { GuildDefaultMessageNotifications } = require("discord.js");
 var fs = require("fs")
 var path = require("path")
 
 // For testing user input
-// const prompt = require("prompt-sync")({sigint: true});
+const prompt = require("prompt-sync")({sigint: true});
 
 // TODO:
 // KEEP TRACK OF USED CHARACTERS and create getters for them
 // CREATE GETTER FOR PAST ATTEMPTS
 
-var WORD;
-const guesses = [];
-
-var char_values = {
-    "correct_character": "correct_character",
-    "incorrect_position":"incorrect_position",
-    "incorrect_character":"incorrect_character",
-    "correct_guess":"correct_guess"
-}
-
-function createGame()
-{
-    const wordList = importWords();
-    WORD = wordList[Math.floor(Math.random()*wordList.length)]
-    // submitGuess(WORD);
-
-}
-
-// function testPlay()
-// {
-//     createGame();
-
-//     let shouldPlay = true;
-//     let i = 0;
-//     while(shouldPlay)
-//     {
-//         var userGuess = prompt("Enter guess: ");
-//         var result = submitGuess(userGuess);
-
-//         // If null then maximum guesses acheived
-//         if(guesses.length > 5)
-//         {
-//             shouldPlay = false;
-//             console.log("Ran out of guesses!")
-            
-//         }// check for correct guess
-//         else if(result.STATE == char_values.correct_guess)
-//         {
-//             shouldPlay = false;
-//             console.log("Correct answer!");
-//         }
-        
-//     }
-
-// }
-
-function importWords(){
-    const data = fs.readFileSync("./words.json", {encoding:"utf8", flag:"r"});
-    const words = JSON.parse(data);
-    return words;
-}
-
- 
-function submitGuess(guess)
-{
-    //console.log(WORD);
-
-    let toAdd = [];
-
-    // The following stores each character of the guess under the key 'char' with the correctness of the character under "STATE"
-    // The state is any value from char_values
-    // the char is the individual characters from the guess
-    // Structure example: {CHAR: 'KEY', STATE: 'STATE FROM char_values' }
-    // NOTE: CHAR: null WHEN STATE IS EQUAL TO correct_guess. THIS IS WHEN THE GUESS WAS CORRECT
-    let isCorrect = true;
-    for(let i = 0; i < guess.length; i++)
-    {
-        let key = guess.charAt(i);
-        if(guess.charAt(i) == WORD.charAt(i))
-        {
-            //console.log("Match! "+guess.charAt(i)+", at position: "+i);
-            toAdd.push({CHAR:key, STATE:char_values.correct_character});
-        }
-        else if(WORD.includes(guess.charAt(i)))
-        {
-            //console.log("Incorrect position at index: "+i);
-            toAdd.push({CHAR:key, STATE:char_values.incorrect_position});
-            isCorrect = false;
-        }
-        else
-        {
-            //console.log("Character '"+guess.charAt(i)+"' Not in word")
-            toAdd.push({CHAR:key, STATE:char_values.incorrect_character});
-            isCorrect = false;
-        }
-
-    }
-
-    if(isCorrect)
-    {
-        toAdd = [];
-        toAdd.push({CHAR:null, STATE:char_values.correct_guess});
-    }
-    
-    // ENABLE THESE PRINT STATEMENTS TO SEE CORE DATA
-    // for(let i = 0; i < toAdd.length; i++)
-    // {
-    //     console.log(toAdd[i]);
-    // } 
-    
-    guesses.push({CORRECT:isCorrect, DATA:toAdd});
-
-    // ENABLE THESE PRINT STATEMENTS TO SEE DATA INSIDE GUESSES
-    // for(let i = 0; i < guesses.length; i++)
-    // {
-    //     console.log(guesses[i]);
-    // } 
-
-    return toAdd;
-}
-
-// get remaining guesses
-
-module.exports = {
-    createGame,
-    submitGuess
+const Result = {
+    CORRECT_CHARACTER: "correct_character",
+    INCORRECT_POSITION: "incorrect_position",
+    INCORRECT_CHARACTER: "incorrect_character",
 };
 
-// Just for testing
-// testPlay();
+const wordList = importWords();
+
+class WordleGame{
+    constructor(){
+        // Choose a random word
+        this.word = wordList[Math.floor(Math.random() * wordList.length)]
+        this.guesses = []
+    }
+
+    submitGuess(guess){
+        let toAdd = [];
+        // The following stores each character of the guess under the key 'char' with the correctness of the character under "STATE"
+        // The state is any value from char_values
+        // the char is the individual characters from the guess
+        // Structure example: {CHAR: 'KEY', STATE: 'STATE FROM char_values' }
+        // NOTE: CHAR: null WHEN STATE IS EQUAL TO correct_guess. THIS IS WHEN THE GUESS WAS CORRECT
+        for(let i = 0; i < guess.length; i++){
+            let key = guess.charAt(i);
+            let result = Result.INCORRECT_CHARACTER;
+
+            if(guess.charAt(i) == this.word.charAt(i))
+                result = Result.CORRECT_CHARACTER;
+            else if(this.word.includes(guess.charAt(i)))
+                result = Result.INCORRECT_POSITION;
+            
+            toAdd.push({char: key, result: result});
+        }
+        
+        let result = ({
+            correct: toAdd.every(r => r.result == Result.CORRECT_CHARACTER), 
+            guess: toAdd,
+        });
+
+        this.guesses.push(result);
+
+        return result;
+    }
+}
+
+function importWords(){
+    let data = fs.readFileSync("./words.json", {encoding:"utf8", flag:"r"});
+    return JSON.parse(data);
+}
+
+// TESTING ONLY
+function testPlay()
+{
+    let game = new WordleGame();
+    console.log(game.word);
+
+    let shouldPlay = true;
+    let i = 0;
+    while(shouldPlay)
+    {
+        let userGuess = prompt("Enter guess: ");
+        let result = game.submitGuess(userGuess);
+        console.log(result);
+
+        // If null then maximum guesses acheived
+        if(game.guesses.length > 5)
+        {
+            shouldPlay = false;
+            console.log("Ran out of guesses!")
+            
+        }// check for correct guess
+        else if(result.correct)
+        {
+            shouldPlay = false;
+            console.log("Correct answer!");
+        }
+        
+    }
+
+}
+
+module.exports = {
+    WordleGame,
+    Result,
+};
+
+// TESTING ONLY
+testPlay();
