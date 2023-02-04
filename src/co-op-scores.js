de7b7f44ff46e4b9eedb6a187965f9a0cf2c90a6
Tworye01 <natanwojcik15@gmail.com>
2023-02-04 13:55:01 +0000

    Score for co-op

---
+++ src/co-op-scores.js
@@ -0,0 +1,17 @@
+var startTime, endTime, tempScore;
+const coopAddScore = 6;
+
+
+
+function start() {
+  startTime = new Date();
+}
+function end() {
+   endTime = new Date();
+   var diffTime = endTime - startTime;
+   return diffTime;
+}
+
+function scoresClac() {
+  
+} 
