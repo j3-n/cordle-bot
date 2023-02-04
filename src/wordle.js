782e4caaad74e323f52cd94db191ce778a2f5d93
Josh <joshuaborges63@yahoo.co.uk>
2023-02-04 14:44:58 +0000

    Added submit guess functionality to wordle class

--- src/wordle.js
+++ src/wordle.js
@@ -1,48 +1,81 @@
 var fs = require("fs")
 var path = require("path")
-// Create new game
 
-// Submit new guesses
-// Enum with guessed characters
-// 
+var WORD;
+const guesses = [];
 
-//process.stdout.write("Hello world!")
+var char_values = {
+    "Correct_Character": "Correct_Character",
+    "Incorrect_Position":"Incorrect_Position",
+    "Incorrect_Character":"Incorrect_Character"
+}
 
-// Create game -> init function
 function createGame()
 {
+    const wordList = importWords();
+    WORD = wordList[Math.floor(Math.random()*wordList.length)]
+    submitGuess("Josh");
+
+}
+
+
+
+function importWords(){
+    const data = fs.readFileSync("./words.json", {encoding:"utf8", flag:"r"});
+    const words = JSON.parse(data);
+    return words;
+}
+
+// Submit guess and send back how good
+// Struct contains array full of 
+function submitGuess(guess)
+{
+    if(guesses.length > 6)
+    {
+        return null;
+    }
+
+    const toAdd = [];
+
+    // The following stores each character of the guess under the key 'char' with the correctness of the character under "STATE"
+    // The state is any value from char_values
+    // the char is the individual characters from the guess
+    for(let i = 0; i < guess.length; i++)
+    {
+        let key = guess.charAt(i);
+        if(guess.charAt(i) == WORD.charAt(i))
+        {
+            console.log("Match! "+guess.charAt(i)+", at position: "+i);
+            toAdd.push({char:key, STATE:char_values.Correct_Character});
+        }
+        else if(WORD.includes(guess.charAt(i)))
+        {
+            console.log("Incorrect position at index: "+i);
+            toAdd.push({char:key, STATE:char_values.Incorrect_Position});
+        }
+        else
+        {
+            console.log("Character '"+guess.charAt(i)+"' Not in word")
+            toAdd.push({char:key, STATE:char_values.Incorrect_Character});
+        }
+
+    }   
     
+    // DEBUG PRINT STATEMENT
+    // for(let i = 0; i < toAdd.length; i++)
+    // {
+    //     console.log(toAdd[i]);
+    // } 
+    guesses.push(toAdd);
+    return toAdd;
 }
 
-function importWords()
-{
-    // Read file
-    fs.readFile("./words.json", "utf8", function (err, data){
-        if(err)
-        {
-            process.stdout.write(err.message);
-            return;
-        }
-        try
-        {
-            // Parse json data into legible words and return
-            const words = JSON.parse(data);
-            return words;
-        }catch (err)
-        {
-            process.stdout.write(err.message+"\n");
-        }
-    })
-}
-
-function submitGuess()
-{
-
-}
+// get remaining guesses
 
 module.exports = {
     createGame,
     submitGuess
 };
 
-importWords();
+// Just for testing
+// createGame();
