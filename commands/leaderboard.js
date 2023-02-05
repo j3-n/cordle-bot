c3ae4bdab5e8c6d3a1fe54cdc5b00813c7c6e8b5
Joseph Beck <joebeck03@icloud.com>
2023-02-05 01:19:01 +0000

    leaderboard command added

---
+++ commands/leaderboard.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { Leaderboard } = require('../src/leaderboard');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("leaderboard")
+        .setDescription("Get a view of the current leaderboard"),
+    async execute(interaction){
+        // create leaderboard
+        const leaderboard = new Leaderboard();
+        leaderboard.initialize();
+        const topTen = leaderboard.makeTopTen();
+
+        var leaderboardStr = "";
+
+        for (let i = 0; i < topTen.length; i++) {
+            leaderboardStr += (`${i + 1}. ${topTen[i].name}:${topTen[i].elo}\n`);
+        }
+
+        interaction.reply({
+            content: leaderboardStr,
+            ephemeral: true
+        });
+    },
+};
