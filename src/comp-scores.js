5a83f70955783e2f775ca95399c36f938261d32e
Tworye01 <natanwojcik15@gmail.com>
2023-02-05 05:21:01 +0000

    Upgraded comp score alg with database

--- src/comp-scores.js
+++ src/comp-scores.js
@@ -1,47 +1,81 @@
 const { start,end} = require('./time.js');
+const { FirebaseFunctions } = require('./firebase/firebase-functions');
 const compMultiplier = 0.5;
 var ogComp = 8; //comp multi
 var compTimeScore =  0;
+const fbFunc = new FirebaseFunctions();
+fbFunc.initialize();
 
-function compScoresCalc() { //calcs 
-    if (end() < 20){
-      for(let i = 0; i < end(); i++){
-        ogComp = ogComp*compMultiplier;
+// function compScoresCalc() { //calcs 
+//     if (end() < 20){
+//       for(let i = 0; i < end(); i++){
+//         ogComp = ogComp*compMultiplier;
      
-      }
-      compTimeScore += ogComp
-    }
-    return Math.round(compTimeScore);
-} 
+//       }
+//       compTimeScore += ogComp
+//     }
+//     return Math.round(compTimeScore);
+// } 
 
-function compWin(turnWin){
+async function compWin(turnWin, userID){
     //boiler-plate
-    
     const turns = [25,22,18,15,12,9];
     let winScore = turns[turnWin-1] 
-    console.log(winScore+compScoresCalc());
-    //return winScore + total comptime calc
-   //calc elo rating 
-
-}
-
-function compLose(User){
-   //I NEED THE DATABASE FOR THIS
-}
   
-//testers
-/* for(let i = 0; i<2; i++){
-    start();
-    compScoresCalc();
-    for(let j = 0; j<30000; j++){
-        console.log(j);
+
+    const user = await fbFunc.getUser(userID, "users");
+    let score = user.score;
+    winScore = score + winScore;
+    fbFunc.updateUser(
+        user.id,
+        user.name,
+        user.gamesWon,
+        user.gamesLost,
+        user.gamesPlayed,
+        user.elo,
+        winScore,
+        "users"
+    );
+}
+
+async function compLose(userID){
+    
+    let loseScore = -18;
+    
+
+    const user = await fbFunc.getUser(userID, "users");
+    let score = user.score;
+    if( score + loseScore < 0){
+        fbFunc.updateUser(
+            user.id,
+            user.name,
+            user.gamesWon,
+            user.gamesLost,
+            user.gamesPlayed,
+            user.elo,
+            0,
+            "users"
+        );
     }
-  
-    end();
+    else{
+    loseScore = score + loseScore;
+    fbFunc.updateUser(
+        user.id,
+        user.name,
+        user.gamesWon,
+        user.gamesLost,
+        user.gamesPlayed,
+        user.elo,
+        loseScore,
+        "users"
+    );
+    }
 }
+//testers
 
-compWin(2);
-   */
+
+//compWin(2,"natan#71912");
+//compLose("natan#71912");
 
 
 //exports
