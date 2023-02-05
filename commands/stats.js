8e43aa9b227ed7b0f28d5c456daa81eda679a3b1
Jacob Padley <jacob5180@hotmail.co.uk>
2023-02-05 09:31:57 +0000

    Fixed stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -10,13 +10,12 @@ module.exports = {
         const statsHandler = new Stats("natan#71912");
         const stats = await statsHandler.makeStats();
 
-        const statsStr = (stats.id + "'s stats:\n" + 
-            "Games won: ", stats.gamesWon, "\n" + 
-            "Games lost: ", stats.gamesLost, "\n" +
-            "Games played: ", stats.gamesPlayed, "\n" +
-            "Player elo: ", stats.elo, "\n" +
-            "Player score: ", stats.score, "\n"
-        );
+        const statsStr = stats.id + "'s stats:\n" + 
+            "Games won: " + stats.gamesWon + "\n" + 
+            "Games lost: " + stats.gamesLost + "\n" +
+            "Games played: " + stats.gamesPlayed + "\n" +
+            "Player elo: " + stats.elo + "\n" +
+            "Player score: " + stats.score + "\n";
 
         interaction.reply({
             content: statsStr,
