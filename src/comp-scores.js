18f0d1d4d734ebc2753d3245b74877264773e2d6
Joseph Beck <joebeck03@icloud.com>
2023-02-05 08:07:52 +0000

    minor

--- src/comp-scores.js
+++ src/comp-scores.js
@@ -3,13 +3,13 @@ const { FirebaseFunctions } = require('./firebase/firebase-functions');
 async function compWin(turnWin, userID){
     //boiler-plate
     const turns = [25,22,18,15,12,9];
-    let winScore = turns[turnWin-1] 
-    let eloScore = turns[turnWin-1] 
+    var winScore = turns[turnWin-1] 
+    var eloScore = turns[turnWin-1] 
 
     const user = await FirebaseFunctions.getUser(userID, "users");
-    let score = user.score;
+    var score = user.score;
     winScore += score;
-    let eloScores = user.elo;
+    var eloScores = user.elo;
     eloScores += eloScore;
 
     return {
