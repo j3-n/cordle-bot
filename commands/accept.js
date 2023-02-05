8a94a027ab5de2c3a3ea688a2c94c0de4d52ead4
Jacob Padley <jacob5180@hotmail.co.uk>
2023-02-05 01:16:38 +0000

    Made threads work good

--- commands/accept.js
+++ commands/accept.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder } = require("discord.js");
-const { newGame, findChallenge, completeChallenge } = require("../game-manager.js");
-const { DuelWordle } = require("../src/duel-game.js");
+const { newGame, findChallenge, completeChallenge, gameExists } = require("../game-manager.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,16 +9,17 @@ module.exports = {
         // Check if a challenge exists for this user
         let challenge = findChallenge(interaction.user.id);
         if(challenge){
-            if(newGame(interaction.channelId, new DuelWordle(challenge.player1, challenge.player2))){
-                completeChallenge(challenge.player1, challenge.player2);
-                interaction.reply({content: "Challenge accepted! Good luck.", ephemeral: true});
-
-                // Create a new thread for the game to take place in
-                interaction.channel.threads.create({
-                    name: `${interaction.user.username}'s game`,
+            let threadName = `${interaction.user.username}'s game`;
+            if(!gameExists(challenge.player1)){
+                // Create a thread for the challenge
+                let thread = await interaction.channel.threads.create({
+                    name: threadName,
                     autoArchiveDuration: 60,
                     reason: `${interaction.user.username} is duelling!`,                    
                 });
+                newGame(thread.id, challenge.player1, challenge.player2);
+                completeChallenge(challenge.player1, challenge.player2);
+                interaction.reply({content: "Challenge accepted! Good luck.", ephemeral: true});
             } else
                 interaction.reply({content: "You are already in a game!", ephemeral: true});
         } else
