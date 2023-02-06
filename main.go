5b5b728e71590a799d0ab3fef1a62130d3e05317
JPadley18 <jacob5180@hotmail.co.uk>
2023-02-06 16:46:41 +0000

    Gave session variable a more descriptive name

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"time" // Temporary
-	
+
 	"cordle/config"
 	"cordle/util"
 
@@ -11,19 +11,21 @@ import (
 
 // Path to read config from
 const ConfigPath = "config/config.json"
+// Path to read command metadata from
+const CommandsPath string = "commands/commands.json"
 
 func main() {
 	// Load config file
 	config := config.LoadConfig(ConfigPath)
 
 	// Start discord bot
-	discord, err := discordgo.New("Bot " + config.Token)
+	session, err := discordgo.New("Bot " + config.Token)
 	util.CheckError(err, "Failed to initialise discord session")
-	discord.Open()
-	defer discord.Close()
+	session.Open()
+	defer session.Close()
 
 	// Set the bot's status
-	err = discord.UpdateGameStatus(0, config.Status)
+	err = session.UpdateGameStatus(0, config.Status)
 	util.CheckError(err, "Failed to set status")
 
 	// Temporary, stops the bot from instantly logging out
