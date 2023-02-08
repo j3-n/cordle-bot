25252c1d7e4cf4ede8d51eb370b479c251da3979
JPadley18 <jacob5180@hotmail.co.uk>
2023-02-08 11:16:04 +0000

    Check for existing duels before starting one

--- commands/duel.go
+++ commands/duel.go
@@ -1,9 +1,21 @@
 package commands
 
 import (
+	"fmt"
+
+	"cordle/game"
+
 	"github.com/bwmarrin/discordgo"
 )
 
 func duel(s *discordgo.Session, i *discordgo.InteractionCreate){
-
+	// Check that the target does not already have a challenge against them
+	user := i.ApplicationCommandData().Options[0].UserValue(s)
+	if game.FindChallenge(user) == nil{
+		// Create a new challenge
+		game.NewChallenge(i.Interaction.Member.User, user)
+		fmt.Println("Made new challenge")
+	} else {
+		fmt.Println("Challenge already exists")
+	}
 }
