449136d945fc1592990b21e6ab99f95fb7ce0116
JPadley18 <jacob5180@hotmail.co.uk>
2023-02-08 10:44:56 +0000

    Added NewChallenge and FindChallenge

--- game/challenge.go
+++ game/challenge.go
@@ -9,3 +9,26 @@ type Challenge struct{
 	Source	*discordgo.User
 	Target	*discordgo.User
 }
+
+// Stores all currently active challenges
+var challenges []*Challenge
+
+// NewChallenge creates a new challenge between two players
+func NewChallenge(s *discordgo.User, t *discordgo.User){
+	challenges = append(challenges, &Challenge{
+		Source: s,
+		Target: t,
+	})
+}
+
+// Locates and returns a challenge between two players given the target user object
+// Returns nil if one is not found
+func FindChallenge(t *discordgo.User) (*Challenge){
+	for _, c := range challenges{
+		if c.Target.ID == t.ID{
+			return c
+		}
+	}
+
+	return nil
+}
