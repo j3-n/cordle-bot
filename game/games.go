d73b7e2c5c8b9f69eb5e8bcd4dd4ab360c803143
JPadley18 <jacob5180@hotmail.co.uk>
2023-02-14 21:01:07 +0000

    Identify games by user structs

--- game/games.go
+++ game/games.go
@@ -1,6 +1,11 @@
 package game
 
-import "cordle/wordle"
+import (
+	"cordle/wordle"
+	"sync"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 // These are the available modes that Cordle can be played in
 type GameMode int
@@ -14,6 +19,12 @@ const (
 // Game encapsulates the information about an individual game of Cordle, regardless of mode
 type Game struct {
 	Mode 	GameMode
-	Players	[]string
+	Players	[]*discordgo.User
 	Games	[]wordle.WordleGame
+}
+
+// List of all current games, thread safe
+var games struct{
+	g 	[]Game
+	mu 	sync.Mutex
 }
