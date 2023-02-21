53f0c3e8c53a2effe6fcfa0de6cd3f0ad36fb4b8
joseph <joebeck03@icloud.com>
2023-02-21 13:25:19 +0000

    test main

--- main.go
+++ main.go
@@ -1,37 +1,17 @@
 package main
 
-import(
-	
-)
+import "cordle/database"
 
 func main() {
-	// dave := database.User{
-	// 	Id:     678618,
-	// 	Name:   "barnard",
-	// 	Wins:   135,
-	// 	Losses: 10,
-	// 	Draws:  50,
-	// 	Games:  195,
-	// 	Elo:    1261,
-	// 	Level:  120}
+	newUser := database.User{
+		Id:     567223,
+		Name:   "felix",
+		Wins:   56,
+		Losses: 18,
+		Draws:  50,
+		Games:  124,
+		Elo:    701,
+		Level:  51}
 
-	// database.AddUser(dave)
-
-	// user := database.GetUser(456123)
-	// fmt.Println(user.ToString())
-
-	// fmt.Println("")
-
-	// stats := database.GetStats(456123)
-	// fmt.Println(stats.ToString())
-
-	// database.DeleteUser(678618)
-
-	// var ids [3]string
-
-	// ids[0] = "'1251'"
-	// ids[1] = "'4561'"
-	// ids[2] = "'1257'"
-
-	// database.DeleteRecords("users", "id=", ids)
+	database.AddUser(newUser)
 }
