0f929dae27f793dbb1a8c617f68cedb719813b25
Jacob Padley <jacob5180@hotmail.co.uk>
2023-02-21 10:20:25 +0000

    Add /surrender

--- commands/commands.go
+++ commands/commands.go
@@ -11,34 +11,38 @@ import (
 // Big list of available commands
 var commands = []*discordgo.ApplicationCommand{
 	{
-		Name: "duel",
+		Name:        "duel",
 		Description: "Send a duel challenge to another player",
 		Options: []*discordgo.ApplicationCommandOption{
 			{
-				Type: discordgo.ApplicationCommandOptionUser,
-				Name: "opponent",
+				Type:        discordgo.ApplicationCommandOptionUser,
+				Name:        "opponent",
 				Description: "The player you wish to challenge",
-				Required: true,
+				Required:    true,
 			},
 		},
 	},
 	{
-		Name: "accept",
+		Name:        "accept",
 		Description: "Accept a duel challenge against you",
 	},
 	{
-		Name: "decline",
+		Name:        "decline",
 		Description: "Decline a duel challenge against you",
 	},
 	{
-		Name: "guess",
+		Name:        "surrender",
+		Description: "Surrender your current game",
+	},
+	{
+		Name:        "guess",
 		Description: "Submit a guess to a game of wordle",
 		Options: []*discordgo.ApplicationCommandOption{
 			{
-				Type: discordgo.ApplicationCommandOptionString,
-				Name: "guess",
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "guess",
 				Description: "Your five-letter guess",
-				Required: true,
+				Required:    true,
 			},
 		},
 	},
@@ -49,47 +53,48 @@ var regCommands []*discordgo.ApplicationCommand
 // Big map linking command names to their handling functions
 // Handler functions are stored in separate go files
 var commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
-	"duel": 	duel,
-	"accept": 	duelAccept,
-	"decline": 	duelDecline,
-	"guess": 	guess,
+	"duel":      duel,
+	"accept":    duelAccept,
+	"decline":   duelDecline,
+	"surrender": surrender,
+	"guess":     guess,
 }
 
 // RegisterCommands registers all command with Discord, this is necessary to allow users to run them
-func RegisterCommands(s *discordgo.Session){
+func RegisterCommands(s *discordgo.Session) {
 	// Initialise the registered command array
 	regCommands = make([]*discordgo.ApplicationCommand, len(commands))
 	// Iterate over each command and register it
-	for i, cmd := range commands{
+	for i, cmd := range commands {
 		c, err := s.ApplicationCommandCreate(s.State.User.ID, "", cmd)
-		util.CheckError(err, "Failed to create command: /" + cmd.Name)
+		util.CheckError(err, "Failed to create command: /"+cmd.Name)
 		// Log the command for later deletion
 		regCommands[i] = c
 	}
 
 	// Create a handler to map commands to their handlers
-	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate){
+	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		h, exists := commandHandlers[i.ApplicationCommandData().Name]
-		if exists{
+		if exists {
 			h(s, i)
 		}
 	})
 }
 
 // ClearCommands deletes all commands created during this session
-func ClearCommands(s *discordgo.Session){
+func ClearCommands(s *discordgo.Session) {
 	// Iterate over every registered command and delete it
-	for _, cmd := range regCommands{
+	for _, cmd := range regCommands {
 		err := s.ApplicationCommandDelete(s.State.User.ID, "", cmd.ID)
 		util.CheckError(err, fmt.Sprintf("Failed to delete command /%s", cmd.Name))
 	}
 }
 
 // respond sends a response to an interaction
-func respond(s *discordgo.Session, i *discordgo.InteractionCreate, message string, ephemeral bool){
+func respond(s *discordgo.Session, i *discordgo.InteractionCreate, message string, ephemeral bool) {
 	// Configure whether the message is ephemeral or not
 	var f discordgo.MessageFlags
-	if ephemeral{
+	if ephemeral {
 		f = discordgo.MessageFlagsEphemeral
 	}
 
@@ -98,7 +103,7 @@ func respond(s *discordgo.Session, i *discordgo.InteractionCreate, message strin
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
 			Content: message,
-			Flags: f,
+			Flags:   f,
 		},
 	})
 }
---
+++ commands/surrender.go
@@ -0,0 +1,8 @@
+package commands
+
+import "github.com/bwmarrin/discordgo"
+
+// surrender allows a player to quit an ongoing game
+func surrender(s *discordgo.Session, i *discordgo.InteractionCreate) {
+
+}
