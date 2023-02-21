41fc7f73e6e659f5ec821eb7b5a2e515ac810c9d
joseph <joebeck03@icloud.com>
2023-02-21 10:46:03 +0000

    fixed logger.go

--- results/logger.go
+++ results/logger.go
@@ -9,19 +9,19 @@ func LogWin(attempts int, player *database.User) {
 	winCalc(attempts, player)
 	fmt.Println(player.ToString())
 
-	// update database
+	database.UpdateUser(player)
 }
 
 func LogLoss(attempts int, player *database.User) {
 	loseCalc(attempts, player)
 	fmt.Println(player.ToString())
 
-	// update database
+	database.UpdateUser(player)
 }
 
 func LogDraw(player *database.User) {
 	drawCalc(player)
 	fmt.Println(player.ToString())
 
-	// update database
+	database.UpdateUser(player)
 }
