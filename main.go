1eba4ec2c1eed280314bc7a59e89d7185b430aa6
Joseph Beck <joebeck03@icloud.com>
2023-02-24 11:47:04 +0000

    testing & new gitignore

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
-import "cordle/database"
+import (
+	"cordle/database"
+	"fmt"
+)
 
 func main() {
-	newUser := database.User{
-		Id:     567223,
-		Name:   "felix",
-		Wins:   56,
-		Losses: 18,
-		Draws:  50,
-		Games:  124,
-		Elo:    701,
-		Level:  51}
+	// newUser := database.User{
+	// 	Id:     567223,
+	// 	Name:   "felix",
+	// 	Wins:   56,
+	// 	Losses: 18,
+	// 	Draws:  50,
+	// 	Games:  124,
+	// 	Elo:    701,
+	// 	Level:  51}
 
-	database.AddUser(newUser)
+	// database.AddUser(newUser)
+
+	user := database.GetUser(456123)
+	fmt.Println(user.ToString())
 }
