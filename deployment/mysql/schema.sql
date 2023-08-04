e6cf24c8d85189b091dac62a1593dc1385d9c377
joseph <joebeck03@icloud.com>
2023-08-04 20:01:19 +0100

    prod cordle db in schema.sql

--- deployment/mysql/schema.sql
+++ deployment/mysql/schema.sql
@@ -1,5 +1,18 @@
 create database cordle_test;
-use cordle_test
+use cordle_test;
+
+create table users (
+	id     varchar(50)     not null primary key,
+	wins   int default 0   not null,
+	losses int default 0   not null,
+	draws  int default 0   not null,
+	elo    int default 500 not null,
+	constraint id
+	    unique (id)
+);
+
+create database cordle;
+use cordle;
 
 create table users (
 	id     varchar(50)     not null primary key,
