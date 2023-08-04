722056c8f751445a9551395cbbc52a355298ca48
joseph <joebeck03@icloud.com>
2023-08-04 20:03:32 +0100

    not needed any longer

--- internal/database/schema.sql
+++
@@ -1,9 +0,0 @@
-create table users (
-	id     varchar(50)     not null primary key,
-	wins   int default 0   not null,
-	losses int default 0   not null,
-	draws  int default 0   not null,
-	elo    int default 1000 not null,
-	constraint id
-	    unique (id)
-);
